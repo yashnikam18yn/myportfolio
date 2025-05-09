8142efcf3ba8154cdfd495ef387ad2e3e6cdf239
Yash Nikam <108448644+yashnikam18yn@users.noreply.github.com>
2025-05-09 19:33:46 +0530

    quick fix

--- src/data/constant.js
+++ src/data/constant.js
@@ -128,7 +128,7 @@ export const Bio = {
         },
         
         {
-          name: "VS Code",
+          name: "VS-Code",
           image:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
         },
